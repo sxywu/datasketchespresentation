12f5b60f5c7f7abfaef0533bc38fed125c5a7105
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-02-28 21:46:23 -0800

    Delete position calculations for line, songs, themes

--- slides/slide_S_02/sketchLines.js
+++ slides/slide_S_02/sketchLines.js
@@ -15,10 +15,6 @@
 		.domain(radiusExtent)
 		.range([radius, radius * 5]);
 
-	// // position lines, songs, themes
-	// var {linePositions, songPositions, diamondPositions} =
-	// 	positionLines(hamiltonLines, hamiltonSongs, hamiltonThemes);
-
 	var simulation = d3.forceSimulation()
 		.force('collide', d3.forceCollide().radius(d => d.radius + 2))
 		.force('x', d3.forceX().x(d => d.focusX))
@@ -97,104 +93,6 @@
 		circles
 			.transition().duration(500)
 			.attr('opacity', 0.25);
-	}
-
-
-	///////////////////////////////////////////////////////////////////////////
-	/////////////////// Position lines and themes /////////////////////////////
-	///////////////////////////////////////////////////////////////////////////
-	function positionLines(lines, songs, diamonds) {
-		var lineSize = 5;
-    var fontSize = 14;
-    var padding = {x: 1, y: lineSize * 5};
-    var s = 1;
-    var x = lineSize * 6;
-    var y = lineSize * 6;
-    var lastLineId = null;
-
-    var songPositions = [];
-		var linePositionsByLineId = {};
-    // make it an object keyed by lineId for the sake of diamondPositions
-    var linePositions = _.map(lines, (line, i) => {
-      var songNum = line.songId;
-      var startLine = parseInt(line.lineId.split(':')[1].split('-')[0], 10);
-      var endLine = parseInt(line.lineId.split(':')[1].split('-')[1], 10) || startLine;
-
-      // if next song
-      if (songNum !== s) {
-        s = songNum;
-        // set positions back to the left
-        x = lineSize * 10;
-        y += padding.y;
-
-        // also add song position
-        songPositions.push(Object.assign(songs[songNum], {
-          x, y
-        }));
-        x += 2 * lineSize;
-        y += fontSize + lineSize;
-      }
-      // and if a song has gone over the width
-      // bring it to next line
-      if (x > width && lastLineId !== line.lineId) {
-        x = lineSize * 12;
-        y += 4 * lineSize + 2;
-      }
-
-      // x-position
-      var focusX = x;
-      var length = lineSize * (endLine - startLine + 2);
-      if (lastLineId !== line.lineId) {
-        // add length to the x-position only if
-        // it's not start of song and different line from the last
-        x += length + padding.x;
-      } else {
-        // if it's the same, set focusX back by length
-        // so that this line overlaps with the last
-        // (they are the same line, different singers)
-        focusX -= length + padding.x;
-      }
-
-      // y-position
-      var focusY = y;
-      var radius = lineSize;
-      if (line.numSingers > 1) {
-        focusY += (lineSize / (line.numSingers - 1) * line.singerIndex) - (lineSize / 2);
-        radius = lineSize / line.numSingers + .25;
-      }
-
-      lastLineId = line.lineId;
-
-			return linePositionsByLineId[line.lineId] = Object.assign(line, {
-        focusX,
-        focusY,
-        trueY: y,
-        radius,
-        fullRadius: lineSize,
-        length,
-        startLine,
-        endLine,
-      });
-    });
-
-    var diamondPositions = _.map(diamonds, (theme) => {
-      var startLine = linePositionsByLineId[theme.startLineId];
-
-      var x = startLine.focusX + (theme.startLine - startLine.startLine) * lineSize;
-      var y = startLine.trueY - 2 * startLine.fullRadius;
-      theme.positions = [{x, y, size: lineSize}];
-
-      if (theme.startLine !== theme.endLine) {
-        var endLine = linePositionsByLineId[theme.startLineId];
-        x = endLine.focusX + (theme.endLine - endLine.startLine) * lineSize;
-        y = endLine.trueY - 2 * endLine.fullRadius;
-        theme.positions.push({x, y, size: lineSize});
-      }
-
-      return theme;
-    });
-
-    return {linePositions, songPositions, diamondPositions};
 	}
 
 	function drawPath(d, showLength) {
